754b8e9cb39136a57dfc9a3d4b07c10e52d5e57e
HMFive <yusufciftci@outlook.com.tr>
2023-01-03 00:33:21 +0300

    added toast msg and confirm pwd

--- frontend/src/pages/Dashboard.js
+++ frontend/src/pages/Dashboard.js
@@ -1,6 +1,7 @@
 import React, {useState, useEffect} from 'react';
 import logo from '../logo.png'
 import {Loader} from "../components/shared/Loader";
+import {Navbar} from "../components/shared/Navbar";
 export function Dashboard() {
     const [loading, setLoading] = useState(false);
 
@@ -12,94 +13,98 @@ export function Dashboard() {
     }, [])
 
     return(
-        loading ? (<Loader/>) : (
-        <section className="bg-gray-50 dark:bg-zinc-900">
-                <div
-                    className="dark flex flex-row gap-48 items-center justify-center px-6 py-8 mx-auto md:h-screen lg:py-0">
-                    <div
-                        className="max-w-sm p-6 bg-white border border-gray-200 rounded-lg shadow-md dark:bg-zinc-700 dark:border-pink-800">
-                        <h1 className="mb-2 text-7xl font-bold tracking-tight text-gray-900 dark:text-white">Abortion
-                            should be legal</h1>
-                        <div className="flex space-x-40">
-                            <button type="button"
-                                    className="text-white bg-gradient-to-r from-green-400 via-green-500 to-green-600 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-green-300 dark:focus:ring-green-800 shadow-lg shadow-green-500/50 dark:shadow-lg dark:shadow-green-800/80 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2">Green
-                            </button>
-                            <button type="button"
-                                    className="text-white bg-gradient-to-r from-red-400 via-red-500 to-red-600 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-red-300 dark:focus:ring-red-800 shadow-lg shadow-red-500/50 dark:shadow-lg dark:shadow-red-800/80 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2">Red
-                            </button>
+        loading ? (<Loader/>) :
+            (
+                <div>
+                    <Navbar/>
+                    <section className="bg-gray-50 dark:bg-zinc-900">
+                        <div
+                            className="dark flex flex-row gap-48 items-center justify-center px-6 py-8 mx-auto md:h-screen lg:py-0">
+                            <div
+                                className="max-w-sm p-6 bg-white border border-gray-200 rounded-lg shadow-md dark:bg-zinc-700 dark:border-pink-800">
+                                <h1 className="mb-2 text-7xl font-bold tracking-tight text-gray-900 dark:text-white">Abortion
+                                    should be legal</h1>
+                                <div className="flex space-x-40">
+                                    <button type="button"
+                                            className="text-white bg-gradient-to-r from-green-400 via-green-500 to-green-600 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-green-300 dark:focus:ring-green-800 shadow-lg shadow-green-500/50 dark:shadow-lg dark:shadow-green-800/80 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2">Green
+                                    </button>
+                                    <button type="button"
+                                            className="text-white bg-gradient-to-r from-red-400 via-red-500 to-red-600 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-red-300 dark:focus:ring-red-800 shadow-lg shadow-red-500/50 dark:shadow-lg dark:shadow-red-800/80 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2">Red
+                                    </button>
+                                </div>
+                            </div>
+
+
+                            <div className="overflow-x-auto border rounded relative dark:border-pink-800">
+                                <table className="w-full text-sm text-left text-gray-500 dark:text-gray-400">
+                                    <thead
+                                        className="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-zinc-800 dark:text-gray-400">
+                                    <tr>
+                                        <th scope="col" className="py-3 px-6">
+                                            Agree
+                                        </th>
+                                        <th scope="col" className="py-3 px-6">
+                                            Disagree
+                                        </th>
+                                    </tr>
+                                    </thead>
+                                    <tbody className>
+                                    <tr className="bg-white border-b dark:bg-zinc-700 dark:border-gray-700">
+                                        <td scope="row"
+                                            className="py-4 px-6 font-medium text-gray-900 dark:text-white">
+                                            There are also economic arguments in favor of legal abortion. Unplanned pregnancies
+                                            can have significant financial consequences for women, particularly if they are not
+                                            able to afford to raise a child. Allowing women to have the option of abortion can
+                                            help to ensure that they are able to plan for their futures and make the best
+                                            decisions for themselves and their families in regards to their economic stability.
+                                        </td>
+                                        <td className="py-4 px-6 font-medium dark:text-white">
+                                            abortion is that it can have negative physical and emotional consequences for the
+                                            woman who undergoes the procedure. Some people argue that abortion can lead to
+                                            physical complications such as infection, bleeding, and damage to the reproductive
+                                            organs, as well as emotional problems such as guilt, anxiety, and depression.
+                                        </td>
+                                    </tr>
+                                    <tr className="bg-white border-b dark:bg-zinc-700 dark:border-gray-700">
+                                        <th scope="row"
+                                            className="py-4 px-6 font-medium text-gray-900 dark:text-white">
+                                            There are also economic arguments in favor of legal abortion. Unplanned pregnancies
+                                            can have significant financial consequences for women, particularly if they are not
+                                            able to afford to raise a child. Allowing women to have the option of abortion can
+                                            help to ensure that they are able to plan for their futures and make the best
+                                            decisions for themselves and their families in regards to their economic stability.
+                                        </th>
+                                        <td className="py-4 px-6">
+                                            There are also economic arguments in favor of legal abortion. Unplanned pregnancies
+                                            can have significant financial consequences for women, particularly if they are not
+                                            able to afford to raise a child. Allowing women to have the option of abortion can
+                                            help to ensure that they are able to plan for their futures and make the best
+                                            decisions for themselves and their families in regards to their economic stability.
+                                        </td>
+                                    </tr>
+                                    <tr className="bg-white dark:bg-zinc-700">
+                                        <th scope="row"
+                                            className="py-4 px-6 font-medium text-gray-900  dark:text-white">
+                                            There are also economic arguments in favor of legal abortion. Unplanned pregnancies
+                                            can have significant financial consequences for women, particularly if they are not
+                                            able to afford to raise a child. Allowing women to have the option of abortion can
+                                            help to ensure that they are able to plan for their futures and make the best
+                                            decisions for themselves and their families in regards to their economic stability.
+                                        </th>
+                                        <td className="py-4 px-6">
+                                            There are also economic arguments in favor of legal abortion. Unplanned pregnancies
+                                            can have significant financial consequences for women, particularly if they are not
+                                            able to afford to raise a child. Allowing women to have the option of abortion can
+                                            help to ensure that they are able to plan for their futures and make the best
+                                            decisions for themselves and their families in regards to their economic stability.
+                                        </td>
+                                    </tr>
+                                    </tbody>
+                                </table>
+                            </div>
                         </div>
-                    </div>
-
-
-                    <div className="overflow-x-auto border rounded relative dark:border-pink-800">
-                        <table className="w-full text-sm text-left text-gray-500 dark:text-gray-400">
-                            <thead
-                                className="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-zinc-800 dark:text-gray-400">
-                            <tr>
-                                <th scope="col" className="py-3 px-6">
-                                    Agree
-                                </th>
-                                <th scope="col" className="py-3 px-6">
-                                    Disagree
-                                </th>
-                            </tr>
-                            </thead>
-                            <tbody className>
-                            <tr className="bg-white border-b dark:bg-zinc-700 dark:border-gray-700">
-                                <td scope="row"
-                                    className="py-4 px-6 font-medium text-gray-900 dark:text-white">
-                                    There are also economic arguments in favor of legal abortion. Unplanned pregnancies
-                                    can have significant financial consequences for women, particularly if they are not
-                                    able to afford to raise a child. Allowing women to have the option of abortion can
-                                    help to ensure that they are able to plan for their futures and make the best
-                                    decisions for themselves and their families in regards to their economic stability.
-                                </td>
-                                <td className="py-4 px-6 font-medium dark:text-white">
-                                    abortion is that it can have negative physical and emotional consequences for the
-                                    woman who undergoes the procedure. Some people argue that abortion can lead to
-                                    physical complications such as infection, bleeding, and damage to the reproductive
-                                    organs, as well as emotional problems such as guilt, anxiety, and depression.
-                                </td>
-                            </tr>
-                            <tr className="bg-white border-b dark:bg-zinc-700 dark:border-gray-700">
-                                <th scope="row"
-                                    className="py-4 px-6 font-medium text-gray-900 dark:text-white">
-                                    There are also economic arguments in favor of legal abortion. Unplanned pregnancies
-                                    can have significant financial consequences for women, particularly if they are not
-                                    able to afford to raise a child. Allowing women to have the option of abortion can
-                                    help to ensure that they are able to plan for their futures and make the best
-                                    decisions for themselves and their families in regards to their economic stability.
-                                </th>
-                                <td className="py-4 px-6">
-                                    There are also economic arguments in favor of legal abortion. Unplanned pregnancies
-                                    can have significant financial consequences for women, particularly if they are not
-                                    able to afford to raise a child. Allowing women to have the option of abortion can
-                                    help to ensure that they are able to plan for their futures and make the best
-                                    decisions for themselves and their families in regards to their economic stability.
-                                </td>
-                            </tr>
-                            <tr className="bg-white dark:bg-zinc-700">
-                                <th scope="row"
-                                    className="py-4 px-6 font-medium text-gray-900  dark:text-white">
-                                    There are also economic arguments in favor of legal abortion. Unplanned pregnancies
-                                    can have significant financial consequences for women, particularly if they are not
-                                    able to afford to raise a child. Allowing women to have the option of abortion can
-                                    help to ensure that they are able to plan for their futures and make the best
-                                    decisions for themselves and their families in regards to their economic stability.
-                                </th>
-                                <td className="py-4 px-6">
-                                    There are also economic arguments in favor of legal abortion. Unplanned pregnancies
-                                    can have significant financial consequences for women, particularly if they are not
-                                    able to afford to raise a child. Allowing women to have the option of abortion can
-                                    help to ensure that they are able to plan for their futures and make the best
-                                    decisions for themselves and their families in regards to their economic stability.
-                                </td>
-                            </tr>
-                            </tbody>
-                        </table>
-                    </div>
+                    </section>
                 </div>
-            </section>
         )
     );
 }
--- frontend/src/pages/Register.js
+++ frontend/src/pages/Register.js
@@ -9,23 +9,29 @@ import {
     updateProfile,
 } from "firebase/auth";
 import logo from "../logo.png"
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 import donut from "../donut.gif";
 
 export function Register(props) {
     const navigate = useNavigate();
 
-
     const [email, setEmail] = useState("");
     const [password, setPassword] = useState("");
+    const [confirmPassword, setConfirmPassword] = useState("");
 
     async function handleSubmit(event){
         event.preventDefault();
-        
+
+        if (password !== confirmPassword) {
+            toast("Passwords do not match", {theme: "dark"});
+        }
+
         try {
             await register(email,password);
             navigate('/dashboard');
         } catch(error) {
-            alert("sadfasdf");
+            toast(error.message, {theme: "dark"});
         }
     };
 
@@ -34,45 +40,57 @@ export function Register(props) {
     }
 
     return (
-        <section className="bg-gray-50 dark:bg-zinc-900">
-            <div className="dark flex flex-col items-center justify-center px-6 py-8 mx-auto md:h-screen lg:py-0">
-                <div
-                    className="w-full bg-white rounded-lg shadow dark:border md:mt-0 sm:max-w-md xl:p-0 dark:bg-zinc-800 dark:border-zinc-700">
-                  
-                    <div className="p-6 space-y-4 md:space-y-6 sm:p-8">
-                    <img src={logo} 
-                         alt="Flowbite Logo"/>
-                        <h1 className="text-xl font-bold leading-tight tracking-tight text-gray-900 md:text-2xl dark:text-white">
-                            Register
-                        </h1>
-                        
-                        <form className="space-y-4 md:space-y-6" action="#" onSubmit={handleSubmit}>
-                            <div>
-                                <label htmlFor="email"
-                                
+        <div>
+            <ToastContainer />
+            <section className="bg-gray-50 dark:bg-zinc-900">
+                <div className="dark flex flex-col items-center justify-center px-6 py-8 mx-auto md:h-screen lg:py-0">
+                    <div
+                        className="w-full bg-white rounded-lg shadow dark:border md:mt-0 sm:max-w-md xl:p-0 dark:bg-zinc-800 dark:border-zinc-700">
 
-                                       className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Your
-                                    email</label>
-                                <input type="email" name="email" id="email"
-                                       onChange={(e) => setEmail(e.target.value)}
-                                       className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-zinc-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-pink-500 dark:focus:border-purple-500"
-                                       placeholder="name@company.com" required=""/>
-                            </div>
-                            <div>
-                                <label htmlFor="password"
-                                       className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Password</label>
-                                <input type="password" name="password" id="password" placeholder="••••••••"
-                                       onChange={(e) => setPassword(e.target.value)}
-                                       className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-zinc-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-pink-500 dark:focus:border-purple-500"
-                                       required=""/>
-                            </div>
-                            <button type="submit"
-                                    className="w-full text-white bg-gradient-to-r from-purple-500 to-pink-500 hover:bg-gradient-to-l focus:ring-4 focus:outline-none focus:ring-purple-200 dark:focus:ring-purple-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2">Register
-                            </button>
-                        </form>
+                        <div className="p-6 space-y-4 md:space-y-6 sm:p-8">
+                            <img src={logo}
+                                 alt="Flowbite Logo"/>
+                            <h1 className="text-xl font-bold leading-tight tracking-tight text-gray-900 md:text-2xl dark:text-white">
+                                Register
+                            </h1>
+
+                            <form className="space-y-4 md:space-y-6" action="#" onSubmit={handleSubmit}>
+                                <div>
+                                    <label htmlFor="email"
+
+
+                                           className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Your
+                                        email</label>
+                                    <input type="email" name="email" id="email"
+                                           onChange={(e) => setEmail(e.target.value)}
+                                           className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-zinc-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-pink-500 dark:focus:border-purple-500"
+                                           placeholder="name@company.com" required=""/>
+                                </div>
+                                <div>
+                                    <label htmlFor="password"
+                                           className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Password</label>
+                                    <input type="password" name="password" id="password" placeholder="••••••••"
+                                           onChange={(e) => setPassword(e.target.value)}
+                                           className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-zinc-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-pink-500 dark:focus:border-purple-500"
+                                           required=""/>
+                                </div>
+                                <div>
+                                    <label htmlFor="confirmPassword"
+                                           className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Confirm Password</label>
+                                    <input type="password" name="confirmPassword" id="confirmPassword" placeholder="••••••••"
+                                           onChange={(e) => setConfirmPassword(e.target.value)}
+                                           className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-zinc-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-pink-500 dark:focus:border-purple-500"
+                                           required=""/>
+                                </div>
+                                <button type="submit"
+                                        className="w-full text-white bg-gradient-to-r from-purple-500 to-pink-500 hover:bg-gradient-to-l focus:ring-4 focus:outline-none focus:ring-purple-200 dark:focus:ring-purple-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2">Register
+                                </button>
+                            </form>
+                        </div>
                     </div>
                 </div>
-            </div>
-        </section>
+            </section>
+        </div>
+
     );
 }
