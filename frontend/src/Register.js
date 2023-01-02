d6a9ff2c3633665a34b798978cd61cd77c986c13
TheHabibi <celebialieren@gmail.com>
2023-01-02 20:15:02 +0300

    fix frontend

--- frontend/src/Register.js
+++ frontend/src/Register.js
@@ -7,6 +7,7 @@ import {
     signOut,
     updateProfile,
 } from "firebase/auth";
+import logo from "./logo.png"
 
 export function Register(props) {
 
@@ -27,17 +28,24 @@ export function Register(props) {
     }
 
     return (
+        
         <section className="bg-gray-50 dark:bg-zinc-900">
+            
             <div className="dark flex flex-col items-center justify-center px-6 py-8 mx-auto md:h-screen lg:py-0">
                 <div
                     className="w-full bg-white rounded-lg shadow dark:border md:mt-0 sm:max-w-md xl:p-0 dark:bg-zinc-800 dark:border-zinc-700">
+                  
                     <div className="p-6 space-y-4 md:space-y-6 sm:p-8">
+                    <img src={logo} 
+                         alt="Flowbite Logo"/>
                         <h1 className="text-xl font-bold leading-tight tracking-tight text-gray-900 md:text-2xl dark:text-white">
                             Register
                         </h1>
+                        
                         <form className="space-y-4 md:space-y-6" action="#" onSubmit={handleSubmit}>
                             <div>
                                 <label htmlFor="email"
+                                
 
                                        className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Your
                                     email</label>
