98afc4170b6adff5e88272a4dec308b54ebb6939
HMFive <yusufciftci@outlook.com.tr>
2023-01-02 19:58:07 +0300

    Initial front-end commit

---
+++ front/donut-share/src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+import {Register} from './Register'
+import {Navbar} from './Navbar'
+import {Dashboard} from './Dashboard'
+function App() {
+  return (
+      <div>
+          <Register></Register>
+      </div>
+  );
+}
+
+export default App;
