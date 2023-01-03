0606aa894ad44e95d399a1366f550664a4a00a69
HMFive <yusufciftci@outlook.com.tr>
2023-01-03 09:27:01 +0300

    Final Commit

--- frontend/src/pages/Add_moderator.js
+++ frontend/src/pages/Add_moderator.js
@@ -12,6 +12,7 @@ import logo from "../logo.png"
 import { ToastContainer, toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 import donut from "../donut.gif";
+import {Navbar} from "../components/shared/Navbar";
 
 export function Add_moderator(props) {
     const navigate = useNavigate();
@@ -28,9 +29,11 @@ export function Add_moderator(props) {
         }
 
         try {
-            alert('The moderator is added');
+
             await register(email,password);
-            navigate('/admin');
+            alert('The moderator is added');
+            navigate('/dashboard');
+
             
         } catch(error) {
             toast(error.message, {theme: "dark"});
@@ -42,56 +45,58 @@ export function Add_moderator(props) {
     }
 
     return (
+
         <div>
-            <ToastContainer />
-            <section className="bg-gray-50 dark:bg-zinc-900">
-                <div className="dark flex flex-col items-center justify-center px-6 py-8 mx-auto md:h-screen lg:py-0">
-                    <div
-                        className="w-full bg-white rounded-lg shadow dark:border md:mt-0 sm:max-w-md xl:p-0 dark:bg-zinc-800 dark:border-zinc-700">
+            <Navbar></Navbar>
+            <div>
+                <ToastContainer />
+                <section className="bg-gray-50 dark:bg-zinc-900">
+                    <div className="dark flex flex-col items-center justify-center px-6 py-8 mx-auto md:h-screen lg:py-0">
+                        <div
+                            className="w-full bg-white rounded-lg shadow dark:border md:mt-0 sm:max-w-md xl:p-0 dark:bg-zinc-800 dark:border-zinc-700">
+                            <div className="p-6 space-y-4 md:space-y-6 sm:p-8">
+                                <h1 className="text-xl font-bold leading-tight tracking-tight text-gray-900 md:text-2xl dark:text-white">
+                                    Add moderator
+                                </h1>
 
-                        <div className="p-6 space-y-4 md:space-y-6 sm:p-8">
-                            <img src={logo}
-                                 alt="Flowbite Logo"/>
-                            <h1 className="text-xl font-bold leading-tight tracking-tight text-gray-900 md:text-2xl dark:text-white">
-                                Add moderator
-                            </h1>
-
-                            <form className="space-y-4 md:space-y-6" action="#" onSubmit={handleSubmit}>
-                                <div>
-                                    <label htmlFor="email"
+                                <form className="space-y-4 md:space-y-6" action="#" onSubmit={handleSubmit}>
+                                    <div>
+                                        <label htmlFor="email"
 
 
-                                           className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Moderator E-mail</label>
-                                    <input type="email" name="email" id="email"
-                                           onChange={(e) => setEmail(e.target.value)}
-                                           className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-zinc-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-pink-500 dark:focus:border-purple-500"
-                                           placeholder="name@company.com" required=""/>
-                                </div>
-                                <div>
-                                    <label htmlFor="password"
-                                           className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Password</label>
-                                    <input type="password" name="password" id="password" placeholder="••••••••"
-                                           onChange={(e) => setPassword(e.target.value)}
-                                           className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-zinc-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-pink-500 dark:focus:border-purple-500"
-                                           required=""/>
-                                </div>
-                                <div>
-                                    <label htmlFor="confirmPassword"
-                                           className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Confirm Password</label>
-                                    <input type="password" name="confirmPassword" id="confirmPassword" placeholder="••••••••"
-                                           onChange={(e) => setConfirmPassword(e.target.value)}
-                                           className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-zinc-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-pink-500 dark:focus:border-purple-500"
-                                           required=""/>
-                                </div>
+                                               className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Moderator E-mail</label>
+                                        <input type="email" name="email" id="email"
+                                               onChange={(e) => setEmail(e.target.value)}
+                                               className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-zinc-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-pink-500 dark:focus:border-purple-500"
+                                               placeholder="name@company.com" required=""/>
+                                    </div>
+                                    <div>
+                                        <label htmlFor="password"
+                                               className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Password</label>
+                                        <input type="password" name="password" id="password" placeholder="••••••••"
+                                               onChange={(e) => setPassword(e.target.value)}
+                                               className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-zinc-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-pink-500 dark:focus:border-purple-500"
+                                               required=""/>
+                                    </div>
+                                    <div>
+                                        <label htmlFor="confirmPassword"
+                                               className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Confirm Password</label>
+                                        <input type="password" name="confirmPassword" id="confirmPassword" placeholder="••••••••"
+                                               onChange={(e) => setConfirmPassword(e.target.value)}
+                                               className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-zinc-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-pink-500 dark:focus:border-purple-500"
+                                               required=""/>
+                                    </div>
 
-                                <button type="submit" class="text-white bg-gradient-to-br from-pink-500 to-orange-400 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-pink-200 dark:focus:ring-pink-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2">Add Moderator</button>
+                                    <button type="submit" class="text-white bg-gradient-to-br from-pink-500 to-orange-400 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-pink-200 dark:focus:ring-pink-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2">Add Moderator</button>
 
-                            </form>
+                                </form>
+                            </div>
                         </div>
                     </div>
-                </div>
-            </section>
+                </section>
+            </div>
         </div>
+
 
     );
 }
--- frontend/src/pages/Admin_panel.js
+++ frontend/src/pages/Admin_panel.js
@@ -1,7 +1,7 @@
 import React, {useState, useEffect} from 'react';
 import logo from '../logo.png'
 import {Loader} from "../components/shared/Loader";
-import {Admin_Navbar} from "../components/shared/admin_navbar";
+import {Navbar} from "../components/shared/Navbar";
 export function Admin_Panel() {
     const [loading, setLoading] = useState(false);
     const [activeTab, setActiveTab] = useState('dashboard');
@@ -16,7 +16,7 @@ export function Admin_Panel() {
   
     return (
       <section className="bg-gray-50 dark:bg-zinc-900">
-      <Admin_Navbar/>
+      <Navbar/>
       
       <div className="dark flex flex-col items-center justify-center px-6 py-8 mx-auto md:h-screen lg:py-0">
       </div>
